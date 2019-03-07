51a71da431d84c29123bed4e4c411d48ef6e8cc9
Jondn Tryniski <jtrynisk@oswego.edu>
2019-03-07 12:30:57 -0500

    adding FileClient and FileServer to git with basic structure

--- Project2/src/Main.java
+++ Project2/src/Main.java
@@ -1,6 +1,26 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    Scanner sc = new Scanner(System.in);
+    FileClient fileClient;
+    FileServer fileServer;
+    String host;
+    int port;
+
+    System.out.println("Welcome to the simple File Server program. \n");
+    System.out.print("In order to get started please input the server the host shall use: ");
+    host = sc.next();
+    System.out.print("Next please enter the port that the client and server shall connect to: ");
+    port = sc.nextInt();
+
+    System.out.println("Host: " + host + " Port: " + port);
+
+    fileClient = new FileClient(host, port);
+    fileServer = new FileServer(port);
+
+
     }
 }
